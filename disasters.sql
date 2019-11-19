a0b6ce349d10dd2a3f794498dd24b6bd224f0704
Macbook <lmchvz@outlook.com>
2019-11-18 20:27:48 -0500

    adding SQL file

---
+++ disasters.sql
@@ -0,0 +1,41 @@
+drop table disasters;
+drop table factors;
+drop table affected;
+
+create table disasters(
+	year int primary key,
+	all_disaster_count int,
+	drought_count int,
+	earthquake_count int,
+	extreme_temp_count int,
+	extreme_weather_count int,
+	flood_count int,
+	impact_count int,
+	landslide_count int,
+	dry_mass_movement_count int,
+	volcanic_count int,
+	wildfire_count int,
+	all_disaster_cost bigint,
+	drought_cost bigint,
+	earthquake_cost bigint,
+	extreme_temp_cost bigint,
+	extreme_weather_cost bigint,
+	flood_cost bigint,
+	impact_cost bigint,
+	landslide_cost bigint,
+	dry_mass_movement_cost bigint,
+	volcanic_cost bigint,
+	wildfire_cost bigint);
+
+create table factors(
+	year int primary key,
+	avg_temp float,
+	emissions int);
+	
+create table affected(
+	year int primary key,
+	deaths int,
+	injured int,
+	damage int,
+	affected bigint,
+	homeless int);
